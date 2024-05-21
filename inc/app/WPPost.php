425746cd7844f90fd352961d8c72e2c683b932cd
dev8 <dev8.sofona@gmail.com>
2024-05-22 01:49:21 +0300

    +images

--- inc/app/WPPost.php
+++ inc/app/WPPost.php
@@ -22,13 +22,16 @@ class WPPost
 
         $product->set_short_description($dataDto->description);
 
+        $attachments = [];
         foreach ($dataDto->images as $image) {
             $downloadRemoteImage = new WPDownloadRemoteImage($image);
-            $attachmentId = $downloadRemoteImage->download();
+            $attachments[] = $downloadRemoteImage->download();
+        }
 
-            if ($attachmentId) {
-                $product->set_image_id($attachmentId);
-            }
+
+        if (!empty($attachments[0])) {
+            $product->set_image_id($attachments[0]);
+            unset($attachments[0]);
         }
 
 //        $product->set_category_ids( array( 19 ) );
@@ -37,6 +40,10 @@ class WPPost
 
         foreach ($dataDto->options as $key => $option) {
             add_post_meta($productId, $option['title'], $option['value']);
+        }
+
+        foreach ($attachments as $key => $attachment) {
+            add_post_meta($productId, "image_{$key}", $attachment);
         }
 
         add_post_meta($productId, 'donor_url', $dataDto->url);
